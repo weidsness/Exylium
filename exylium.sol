54ccd9c0da4974cba5d50db36b973ba3166ab9cb
weidsness <weids1@ocdsb.ca>
2025-04-02 08:58:59 -0400

    Create exylium.sol
    create basis

---
+++ exylium.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Exylium is ERC20 {
+    constructo(unit256 initialSupply) ERC20("Exylium", "EXLM") {
+      _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+}
